27183b005d1dfb099d5e1595e12fdef6a265052f
ConnorODonnell <connor.odonnell625@gmail.com>
2018-02-16 20:49:41 -0800

    New Homepages
    Added descriptions to homepages

--- 462Project/462input_handler.php
+++ 462Project/462input_handler.php
@@ -1,23 +1,24 @@
 <?php
+session_start();
 include 'initiate_db.php';
 if(isset($_POST['login_submit'])){
   $username=$_POST['username'];
   $password=$_POST['password'];
-  $query = "SELECT COUNT(*), username, password, role FROM (SELECT username, password, role FROM users WHERE username = '".$username."' && password = '".$password."') AS x";
+  $query = "SELECT COUNT(*), username, password, fname, lname, role FROM (SELECT username, password, fname, lname, role FROM users WHERE username = '".$username."' && password = '".$password."') AS x";
   $stmt = $db->prepare($query);
   $stmt->execute();
   $result = $stmt->fetchAll();
 
   foreach ($result as $row){
     if($row['COUNT(*)'] > 0){
+      $_SESSION['user'] = $username;
+      $_SESSION['fname'] = $row['fname'];
+      $_SESSION['lname'] = $row['lname'];
       if($row['role'] == "admin"){
-        $_SESSION['user'] = $username;
         header('Location:http://localhost/462Project/admin_homepage.html.php');
       }elseif($row['role'] == "manager"){
-        $_SESSION['user'] = $username;
         header('Location:http://localhost/462Project/manager_homepage.html.php');
       }elseif($row['role'] == "employee"){
-        $_SESSION['user'] = $username;
         header('Location:http://localhost/462Project/employee_homepage.html.php');
       }
     } else{
--- 462Project/manager_homepage.html.php
+++ 462Project/manager_homepage.html.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
+<?php session_start(); ?>
 <html>
   <head>
     <meta charset="utf-8">
     <title>Manager Homepage</title>
   </head>
   <body>
-
+    Hello <?php echo $_SESSION['fname'] ?> <?php echo $_SESSION['lname']; ?>.  Welcome to the manager homepage.
   </body>
 </html>
