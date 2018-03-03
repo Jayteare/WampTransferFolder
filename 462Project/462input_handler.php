e38d1af2aa3c975b0a6525bc09a64d8aa7ccef00
ConnorODonnell <connor.odonnell625@gmail.com>
2018-03-03 07:31:18 -0800

    Create Company + Basic CSS
    Added the "create a company" functionality as well as a basic CSS layout

--- 462Project/462input_handler.php
+++ 462Project/462input_handler.php
@@ -43,19 +43,19 @@
 
   //Activated when "Create New Employee Account" is pressed on index.html.php
   if(isset($_POST['create_employee_submit'])){
-	  $new_employee_username=$_POST['new_employee_username'];
-	  $new_employee_password=$_POST['new_employee_password'];
-    $new_employee_fname=$_POST['new_employee_fname'];
-	  $new_employee_lname=$_POST['new_employee_lname'];
-    $new_employee_pin=$_POST['new_employee_pin'];
-	  $new_employee_email=$_POST['new_employee_email'];
-    $new_employee_phone=$_POST['new_employee_phone'];
+	  $_SESSION['new_employee_username']=$_POST['new_employee_username'];
+	  $_SESSION['new_employee_password']=$_POST['new_employee_password'];
+    $_SESSION['new_employee_fname']=$_POST['new_employee_fname'];
+	  $_SESSION['new_employee_lname']=$_POST['new_employee_lname'];
+    $_SESSION['new_employee_pin']=$_POST['new_employee_pin'];
+	  $_SESSION['new_employee_email']=$_POST['new_employee_email'];
+    $_SESSION['new_employee_phone']=$_POST['new_employee_phone'];
     /*
     Query determines if username provided is already in database, if
     so currently submitted values are stored in $_SESSION to be displayed on
     create_new_employee.html.php
     */
-    $checkquery = "SELECT COUNT(*) FROM (SELECT username FROM employees WHERE username = '".$new_employee_username."') AS x";
+    $checkquery = "SELECT COUNT(*) FROM (SELECT username FROM employees WHERE username = '".$_SESSION['new_employee_username']."' AND company_pin = '".$_SESSION['new_employee_pin']."') AS x";
     $checkstmt = $db->prepare($checkquery);
     $checkstmt->execute();
     $checkresult = $checkstmt->fetchAll();
@@ -67,34 +67,94 @@
         PIN. If PIN doesn match currently submitted values are stored in
         $_SESSION to be displayed on create_new_employee.html.php
         */
-        $query = "SELECT COUNT(*) FROM (SELECT pin FROM managers WHERE pin = '".$new_employee_pin."') AS x";
+        $query = "SELECT COUNT(*) FROM (SELECT idcompany FROM company WHERE idcompany = '".$_SESSION['new_employee_pin']."') AS x";
         $stmt = $db->prepare($query);
         $stmt->execute();
         $result = $stmt->fetchAll();
         foreach($result as $row){
           if($row['COUNT(*)'] > 0){
-            $query = "INSERT INTO employees VALUES ('".$new_employee_username."', '".$new_employee_password."', '".$new_employee_fname."', '".$new_employee_lname."', '".$new_employee_pin."', '".$new_employee_email."', '".$new_employee_phone."')";
+            $query = "INSERT INTO employees VALUES ('".$_SESSION['new_employee_username']."', '".$_SESSION['new_employee_password']."', '".$_SESSION['new_employee_fname']."', '".$_SESSION['new_employee_lname']."', '".$_SESSION['new_employee_pin']."', '".$_SESSION['new_employee_email']."', '".$_SESSION['new_employee_phone']."')";
     	      $stmt = $db->prepare($query);
     	      $stmt->execute();
+            $_SESSION['new_employee_username']=null;
+            $_SESSION['new_employee_password']=null;
+            $_SESSION['new_employee_fname']=null;
+            $_SESSION['new_employee_lname']=null;
+            $_SESSION['new_employee_pin']=null;
+            $_SESSION['new_employee_email']=null;
+            $_SESSION['new_employee_phone']=null;
+            $_SESSION['employeeCreateErrorMsg']=null;
     	      header('Location:http://localhost/462Project/index.html.php');
           }else{
-            $_SESSION['newuser']=$new_employee_username;
-            $_SESSION['newpass']=$new_employee_password;
-            $_SESSION['newfname']=$new_employee_fname;
-            $_SESSION['newlname']=$new_employee_lname;
-            $_SESSION['newemail']=$new_employee_email;
-            $_SESSION['newphone']=$new_employee_phone;
+            $_SESSION['employeeCreateErrorMsg']="Your PIN must be valid!";
             header('Location:http://localhost/462Project/create_new_employee.html.php');
           }
         }
       }else{
-        $_SESSION['newpass']=$new_employee_password;
-        $_SESSION['newfname']=$new_employee_fname;
-        $_SESSION['newlname']=$new_employee_lname;
-        $_SESSION['newpin']=$new_employee_pin;
-        $_SESSION['newemail']=$new_employee_email;
-        $_SESSION['newphone']=$new_employee_phone;
+        $_SESSION['employeeCreateErrorMsg']="Your Username must be unique!";
         header('Location:http://localhost/462Project/create_new_employee.html.php');
+      }
+    }
+  }
+
+  if(isset($_POST['create_company_submit'])){
+	  $_SESSION['new_company_title']=$_POST['new_company_title'];
+    $_SESSION['new_company_pin']=$_POST['new_company_pin'];
+	  $_SESSION['new_owner_username']=$_POST['new_owner_username'];
+    $_SESSION['new_owner_password']=$_POST['new_owner_password'];
+    $_SESSION['new_owner_fname']=$_POST['new_owner_fname'];
+    $_SESSION['new_owner_lname']=$_POST['new_owner_lname'];
+    $_SESSION['new_owner_fullname']=$_POST['new_owner_fname'] . " " . $_POST['new_owner_lname'];
+	  $_SESSION['new_owner_email']=$_POST['new_owner_email'];
+    $_SESSION['new_owner_phone']=$_POST['new_owner_phone'];
+    /*
+    Query determines if company name provided is already in database, if
+    so currently an error message is stored in $_SESSION to be displayed on
+    create_new_company.html.php
+    */
+    $checkquery = "SELECT COUNT(*) FROM (SELECT name FROM company WHERE name = '".$_SESSION['new_company_title']."') AS x";
+    $checkstmt = $db->prepare($checkquery);
+    $checkstmt->execute();
+    $checkresult = $checkstmt->fetchAll();
+
+    foreach ($checkresult as $checkrow) {
+      if($checkrow['COUNT(*)'] < 1){
+        /*
+        Query determines if PIN provided already exists. If PIN isn't unique an error message is stored in
+        $_SESSION to be displayed on create_new_company.html.php
+        */
+        $query = "SELECT COUNT(*) FROM (SELECT idcompany FROM company WHERE idcompany = '".$_SESSION['new_company_pin']."') AS x";
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        foreach($result as $row){
+          if($row['COUNT(*)'] > 0){
+            $_SESSION['companyCreateErrorMsg']="Your company PIN must be unique!";
+            header('Location:http://localhost/462Project/create_new_company.html.php');
+          }else{
+            $query = "INSERT INTO company VALUES ('".$_SESSION['new_company_pin']."', '".$_SESSION['new_company_title']."', '".$_SESSION['new_owner_fullname']."')";
+            $stmt = $db->prepare($query);
+            $stmt->execute();
+
+            $query = "INSERT INTO owners VALUES ('".$_SESSION['new_owner_username']."', '".$_SESSION['new_owner_password']."', '".$_SESSION['new_owner_fname']."', '".$_SESSION['new_owner_lname']."', '".$_SESSION['new_owner_email']."', '".$_SESSION['new_owner_phone']."', '".$_SESSION['new_company_pin']."')";
+            $stmt = $db->prepare($query);
+            $stmt->execute();
+            $_SESSION['companyCreateErrorMsg']=null;
+            $_SESSION['new_company_title']=null;
+            $_SESSION['new_company_pin']=null;
+            $_SESSION['new_owner_username']=null;
+            $_SESSION['new_owner_password']=null;
+            $_SESSION['new_owner_fname']=null;
+            $_SESSION['new_owner_lname']=null;
+            $_SESSION['new_owner_pin']=null;
+            $_SESSION['new_owner_email']=null;
+            $_SESSION['new_owner_phone']=null;
+            header('Location:http://localhost/462Project/index.html.php');
+          }
+        }
+      }else{
+        $_SESSION['companyCreateErrorMsg']="Your company name must be unique!";
+        header('Location:http://localhost/462Project/create_new_company.html.php');
       }
     }
   }
--- 462Project/index.html.php
+++ 462Project/index.html.php
@@ -8,18 +8,25 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
+    <link rel="stylesheet" href="stylesheets/styles.css">
     <title>Login</title>
   </head>
   <body>
-    Please enter your username and password
-    <form action="/462Project/462input_handler.php" method="post">
-      Username:<br>
-      <input type="text" name="username"><br>
-      Password:<br>
-      <input type="password" name="password" ><br><br>
-      <input type="submit" name="login_submit" value="Submit">
-    </form>
-    <br>
-    <a href="/462Project/create_new_employee.html.php">Create New Employee Account</a>
+    <main class="main-content">
+      <div class="content-box">
+        <span class="title-text">Placeholder Scheduling Solutions</span>
+        <form action="/462Project/462input_handler.php" method="post">
+          Username:<br>
+          <input type="text" name="username"><br>
+          Password:<br>
+          <input type="password" name="password" ><br><br>
+          <input type="submit" name="login_submit" value="Submit">
+        </form>
+        <br>
+        <button class="link-button" onclick="window.location.href='/462Project/create_new_employee.html.php'">Enroll New Employee</button>
+        <button class="link-button" onclick="window.location.href='/462Project/create_new_company.html.php'">Enroll New Company</button>
+      </div>
+    </main>
   </body>
 </html>
